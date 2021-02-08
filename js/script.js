196c2be04ab07c5ebdb5559b94e0457fd7be69db
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-02-08 15:01:40 +0100

    basic implementation

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    mails: []
+  },
+  mounted() {
+    for(let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((result) => {
+        this.mails.push(result.data.response);
+      });
+    }
+  }
+});
